770a09e7ace01d9c7de89e7e7512f4bd3e6b8f21
shreejan shrestha <shr.shreejan@gmail.com>
2024-10-22 08:41:38 +0545

    Added markup

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,30 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	type Post = {
+		title: string;
+		content: string;
+	};
+
+	let posts = $state<Post[]>([]);
+	let selected = $state();
+	let searchString = $state('');
+</script>
+
+<main class="container flex flex-col gap-4 m-4">
+	<div class="search-container">
+		<label for="search">
+			<span>Search</span>
+			<input type="text" bind:value={searchString} />
+		</label>
+	</div>
+	<div class="post-container">
+		<label for="posts">
+			{#if posts}
+				<select bind:value={selected}>
+					{#each posts as p}
+						<option value={p}>p.title</option>
+					{/each}
+				</select>
+			{/if}
+		</label>
+	</div>
+</main>
